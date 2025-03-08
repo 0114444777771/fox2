c7ee039ea11bc6eab97659a46ed282d3bdd622fb
0114444777771 <am6887744466@gmail.com>
2025-03-08 23:14:26 +0200

    Update Apple.py

--- SrcMusicKERO/platforms/Apple.py
+++ SrcMusicKERO/platforms/Apple.py
@@ -3,7 +3,7 @@ from typing import Union
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.__future__ import VideosSearch
+from  youtube-search-python.__future__ import VideosSearch
 
 
 class AppleAPI:
