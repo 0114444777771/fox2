c783f884dfabfea17de42ce80f6ec6254e51a0ed
0114444777771 <am6887744466@gmail.com>
2025-03-09 19:53:37 +0200

    Update bot.py

--- SrcMusicKERO/core/bot.py
+++ SrcMusicKERO/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class Zelzaly(Client):
     def __init__(self):
-        LOGGER("ميوزك الــيكـس").info(f"جارِ بدء تشغيل البوت . . .")
+        LOGGER("ميوزك فوكس").info(f"جارِ بدء تشغيل البوت . . .")
         super().__init__(
             name="SrcMusicKERO",
             api_id=config.API_ID,
@@ -48,7 +48,7 @@ class Zelzaly(Client):
                 "» قم برفـع البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
             )
             exit()
-        LOGGER("ميوزك اليــكس").info(f" تم بدء تشغيل البوت {self.name} ...✓")
+        LOGGER("ميوزك فوكس").info(f" تم بدء تشغيل البوت {self.name} ...✓")
 
     async def stop(self):
         await super().stop()
