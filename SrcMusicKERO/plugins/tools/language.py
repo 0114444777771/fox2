7f62e025ea685b4393dff18f47d8a79853e35144
0114444777771 <am6887744466@gmail.com>
2025-03-09 23:36:17 +0200

    Update language.py

--- SrcMusicKERO/plugins/tools/language.py
+++ SrcMusicKERO/plugins/tools/language.py
@@ -9,25 +9,29 @@ from config import BANNED_USERS
 from strings import get_string, languages_present
 
 
-def lanuages_keyboard(_):
+def languages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.add(
         *[
-            (
-                InlineKeyboardButton(
-                    text=languages_present[i],
-                    callback_data=f"languages:{i}",
-                )
+            InlineKeyboardButton(
+                text=languages_present[i],
+                callback_data=f"languages:{i}",
             )
             for i in languages_present
         ]
     )
     keyboard.row(
         InlineKeyboardButton(
+            text="🌍 اختيار اللغة",
+            callback_data="change_lang",
+        )
+    )
+    keyboard.row(
+        InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data="settingsback_helper",
         ),
-        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
     )
     return keyboard
 
@@ -35,7 +39,7 @@ def lanuages_keyboard(_):
 @app.on_message(filters.command(["lang", "setlang", "language"]) & ~BANNED_USERS)
 @language
 async def langs_command(client, message: Message, _):
-    keyboard = lanuages_keyboard(_)
+    keyboard = languages_keyboard(_)
     await message.reply_text(
         _["lang_1"],
         reply_markup=keyboard,
@@ -44,24 +48,24 @@ async def langs_command(client, message: Message, _):
 
 @app.on_callback_query(filters.regex("LG") & ~BANNED_USERS)
 @languageCB
-async def lanuagecb(client, CallbackQuery, _):
+async def language_cb(client, CallbackQuery, _):
     try:
         await CallbackQuery.answer()
     except:
         pass
-    keyboard = lanuages_keyboard(_)
+    keyboard = languages_keyboard(_)
     return await CallbackQuery.edit_message_reply_markup(reply_markup=keyboard)
 
 
 @app.on_callback_query(filters.regex(r"languages:(.*?)") & ~BANNED_USERS)
 @ActualAdminCB
 async def language_markup(client, CallbackQuery, _):
-    langauge = (CallbackQuery.data).split(":")[1]
+    language = (CallbackQuery.data).split(":")[1]
     old = await get_lang(CallbackQuery.message.chat.id)
-    if str(old) == str(langauge):
+    if str(old) == str(language):
         return await CallbackQuery.answer(_["lang_4"], show_alert=True)
     try:
-        _ = get_string(langauge)
+        _ = get_string(language)
         await CallbackQuery.answer(_["lang_2"], show_alert=True)
     except:
         _ = get_string(old)
@@ -69,6 +73,18 @@ async def language_markup(client, CallbackQuery, _):
             _["lang_3"],
             show_alert=True,
         )
-    await set_lang(CallbackQuery.message.chat.id, langauge)
-    keyboard = lanuages_keyboard(_)
+    await set_lang(CallbackQuery.message.chat.id, language)
+    keyboard = languages_keyboard(_)
     return await CallbackQuery.edit_message_reply_markup(reply_markup=keyboard)
+
+
+# زر اختيار اللغة
+@app.on_callback_query(filters.regex("change_lang") & ~BANNED_USERS)
+@languageCB
+async def change_language_callback(client, CallbackQuery, _):
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    keyboard = languages_keyboard(_)
+    await CallbackQuery.edit_message_reply_markup(reply_markup=keyboard)
