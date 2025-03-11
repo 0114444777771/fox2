3a57a4d9b8f4a227b9319f9765a2116efee1b842
0114444777771 <am6887744466@gmail.com>
2025-03-11 20:46:03 +0200

    Update mongo.py

--- SrcMusicKERO/core/mongo.py
+++ SrcMusicKERO/core/mongo.py
@@ -1,14 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
+import traceback
 from config import MONGO_DB_URI
-
 from ..logging import LOGGER
 
 LOGGER.info("ميوزك فوكس - جـارِ الاتصـال بقاعـدة البيانـات . . .")
+
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.myDatabase
-    LOGGER("ميوزك فوكس").info("تم الاتصـال بقاعـدة البيانـات ...✓")
-except:
-    LOGGER.error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
+    mongodb = _mongo_async_["fox_database"]  # غيّر "fox_database" إلى اسم القاعدة الفعلي
+    LOGGER.info("تم الاتصـال بقاعـدة البيانـات ...✓")
+except Exception as e:
+    LOGGER.error(f"حدث خطأ أثناء الاتصال بقاعدة البيانات:\n{traceback.format_exc()}")
     exit()
