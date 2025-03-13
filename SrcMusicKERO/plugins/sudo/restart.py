da42f55d5e94c0633acf8b39fa3dea17537214c9
0114444777771 <am6887744466@gmail.com>
2025-03-13 21:39:48 +0200

    Update restart.py

--- SrcMusicKERO/plugins/sudo/restart.py
+++ SrcMusicKERO/plugins/sudo/restart.py
@@ -12,7 +12,7 @@ from pyrogram import filters
 
 import config
 from SrcMusicKERO import app
-from SrcMusicKERO.misc import HAPP, SUDOERS, XCB
+from SrcMusicKERO.misc import HAPP, SUDOERS
 from SrcMusicKERO.utils.database import (
     get_active_chats,
     remove_active_chat,
@@ -23,10 +23,8 @@ from SrcMusicKERO.utils.pastebin import ZelzalyBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-
 async def is_heroku():
     return "heroku" in socket.getfqdn()
-
 
 @app.on_message(command(["getlog", "logs", "السجلات"]) & SUDOERS)
 @language
@@ -36,7 +34,6 @@ async def log_(client, message, _):
     except:
         await message.reply_text(_["server_1"])
 
-
 @app.on_message(command(["تحديث", "حدث"]) & SUDOERS)
 @language
 async def update_(client, message, _):
@@ -44,95 +41,70 @@ async def update_(client, message, _):
         if HAPP is None:
             return await message.reply_text(_["server_2"])
     response = await message.reply_text(_["server_3"])
+    
     try:
         repo = Repo()
-    except GitCommandError:
+    except (GitCommandError, InvalidGitRepositoryError):
         return await response.edit(_["server_4"])
-    except InvalidGitRepositoryError:
-        return await response.edit(_["server_5"])
-    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
-    os.system(to_exc)
-    await asyncio.sleep(7)
-    verification = ""
-    REPO_ = repo.remotes.origin.url.split(".git")[0]
-    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        verification = str(checks.count())
-    if verification == "":
-        return await response.edit(_["server_6"])
-    updates = ""
-    ordinal = lambda format: "%d%s" % (
-        format,
-        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
-    )
-    for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>➣ يوجد تحديث جديد للبوت</b>\n➣ قم بتحديث بوتك الان\n\n<b>➣ التحديثات :</b>\n"
-    _final_updates_ = _update_response_ + updates
-    if len(_final_updates_) > 4096:
-        url = await ZelzalyBin(updates)
-        nrs = await response.edit(
-            f"<b>➣ يوجد تحديث جديد للبوت</b>\n➣ قم بتحديث بوتك الان\n\n<b>➣ التحديثات :</b>\n\n<a href={url}>اضغط هنا لرؤية التحديثات</a>"
-        )
-    else:
-        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
-    os.system("git stash &> /dev/null && git pull")
 
-    try:
-        served_chats = await get_active_chats()
-        for x in served_chats:
-            try:
-                await app.send_message(
-                    chat_id=int(x),
-                    text=_["server_8"].format(app.mention),
-                )
-                await remove_active_chat(x)
-                await remove_active_video_chat(x)
-            except:
-                pass
-        await response.edit(f"{nrs.text}\n\n{_['server_7']}")
-    except:
-        pass
+    os.system(f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null")
+    await asyncio.sleep(7)
+    
+    updates = ""
+    repo_url = repo.remotes.origin.url.split(".git")[0]
+    for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
+        updates += f"<b>➣ {info.summary}</b> - {info.author}\n"
+
+    if not updates:
+        return await response.edit(_["server_6"])
+
+    update_text = f"<b>➣ يوجد تحديث جديد للبوت</b>\n\n<b>➣ التحديثات :</b>\n{updates}"
+    if len(update_text) > 4096:
+        url = await ZelzalyBin(updates)
+        update_text = f"<b>➣ يوجد تحديث جديد للبوت</b>\n\n<a href={url}>اضغط هنا لرؤية التحديثات</a>"
+
+    await response.edit(update_text, disable_web_page_preview=True)
+    os.system("git stash && git pull")
+
+    served_chats = await get_active_chats()
+    for x in served_chats:
+        try:
+            await app.send_message(chat_id=int(x), text=_["server_8"].format(app.mention))
+            await remove_active_chat(x)
+            await remove_active_video_chat(x)
+        except:
+            pass
 
     if await is_heroku():
         try:
-            os.system(
-                f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
-            )
+            os.system("git push heroku master")
             return
         except Exception as err:
-            await response.edit(f"{nrs.text}\n\n{_['server_9']}")
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=_["server_10"].format(err),
-            )
+            await response.edit(f"{update_text}\n\n{_['server_9']}")
+            return await app.send_message(chat_id=config.LOGGER_ID, text=_["server_10"].format(err))
     else:
         os.system("pip3 install -r requirements.txt")
-        os.system(f"kill -9 {os.getpid()} && bash start")
-        exit()
-
+        os.system(f"kill -9 {os.getpid()} && exec bash start.sh")
 
 @app.on_message(command(["اعاده تشغيل"]) & SUDOERS)
 async def restart_(_, message):
     response = await message.reply_text("- جـارِ إعـادة التشغيـل ...")
-    ac_chats = await get_active_chats()
-    for x in ac_chats:
+    
+    active_chats = await get_active_chats()
+    for x in active_chats:
         try:
-            await app.send_message(
-                chat_id=int(x),
-                text=f"» {app.mention} في وضـع اعـادة التشغيـل ...\n\n» انتظـر 20-15 دقيقـه ⏳. . .",
-            )
+            await app.send_message(chat_id=int(x), text=f"» {app.mention} في وضع إعادة التشغيل...")
             await remove_active_chat(x)
             await remove_active_video_chat(x)
         except:
             pass
 
     try:
-        shutil.rmtree("downloads")
-        shutil.rmtree("raw_files")
-        shutil.rmtree("cache")
+        shutil.rmtree("downloads", ignore_errors=True)
+        shutil.rmtree("raw_files", ignore_errors=True)
+        shutil.rmtree("cache", ignore_errors=True)
     except:
         pass
-    await response.edit_text(
-        "» جـارِ اعـادة تشغيـل البـوت ...\n» انتظـر عـدة دقـائـق ⏳\n» حتـى يعمـل البـوت ☑️..."
-    )
-    os.system(f"kill -9 {os.getpid()} && bash start")
+    
+    await response.edit_text("» جـارِ اعـادة تشغيـل البـوت ...\n» انتظـر ⏳...")
+    os.system(f"kill -9 {os.getpid()} && exec bash start.sh")
